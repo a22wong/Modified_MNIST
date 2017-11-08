c4f4fc46202e974ab47275ee9cd04b39f9bf2502
a22wong <a22wong.zm@gmail.com>
2017-11-08 14:43:50 -0500

    "still trying to to play with this stuff"

---
+++ Data/preprocess1.py
@@ -0,0 +1,31 @@
+import csv
+import numpy as np
+import matplotlib.pyplot as plt
+
+train_x = "../Data/train_x.csv"
+train_y = "../Data/train_x.csv"
+train_x_short = "../Data/train_x_short.csv"
+
+
+def reprocess():
+    with open(train_x, 'r+') as readfile:
+        with open(train_x_short, 'w') as writefile:
+            reader = csv.reader(readfile)
+            writer = csv.writer(writefile)
+            count = 0
+            for row in reader:
+                writer.writerow(row)
+                count += 1
+                if count == 100:
+                    break
+
+x = np.loadtxt("../Data/train_x_short.csv", delimiter=",") # load from text
+y = np.loadtxt("../Data/train_y.csv", delimiter=",")
+print "Raw:"
+print x[0]
+x = x.reshape(-1, 64, 64) # reshape
+y = y.reshape(-1, 1)
+print "Reshaped"
+print x[0]
+plt.imshow(x[0]) # to visualize only
+plt.show()
