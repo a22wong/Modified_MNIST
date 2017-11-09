b5075085c6fe553d3aa78b1003f7d6d620da559a
Ryan Burgett <thesonicdoctor@gmail.com>
2017-11-09 13:18:15 -0500

    Revert ""image generation""
    This reverts commit c5d7b2d1cb37625aec7b514eaac32987bb528a2c.

--- Data/preprocess1.py
+++ Data/preprocess1.py
@@ -1,4 +1,4 @@
-from subprocess import call
+import csv
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -7,32 +7,37 @@ train_y = "../Data/train_x.csv"
 train_x_short = "../Data/train_x_short.csv"
 train_y_short = "../Data/train_y_short.csv"
 
-def callHead(n, outfile_x, outfile_y):
-    n = "-"+str(n)
-    call(["head", n, train_x, ">", outfile_x])
-    call(["head", n, train_y, ">", outfile_y])
+def reprocess():
+    with open(train_x, 'r+') as readfile:
+        with open(train_x_short, 'w') as writefile:
+            reader = csv.reader(readfile)
+            writer = csv.writer(writefile)
+            count = 0
+            for row in reader:
+                writer.writerow(row)
+                count += 1
+                if count == 10:
+                    break
 
-def callTail(n, outfile_x, outfile_y):
-    n = "-"+str(n)
-    call(["tail", n, train_x, ">", outfile_x])
-    call(["tail", n, train_y, ">", outfile_y])
+    with open(train_y, 'r+') as readfile:
+        with open(train_y_short, 'w') as writefile:
+            reader = csv.reader(readfile)
+            writer = csv.writer(writefile)
+            count = 0
+            for row in reader:
+                writer.writerow(row)
+                count += 1
+                if count == 10:
+                    break
 
-def callRandom(n):
-    return
-
-def main():
-    # callHead(200, train_x_short, train_y_short)
-    x = np.loadtxt("../Data/train_x_short.csv", delimiter=",") # load from text
-    y = np.loadtxt("../Data/train_y_short.csv", delimiter=",")
-    x = x.reshape(-1, 64, 64) # reshape
-    y = y.reshape(-1, 1)
-
-    plot_n = 2
-    for index, (image, label) in enumerate(zip(x[0:plot_n], y[0:plot_n])):
-        plt.subplot(1,plot_n,index+1)
-        plt.title("Training: %i\n" % label)
-        plt.imshow(x[index])
-    plt.show()
-
-if __name__ == "__main__":
-    main()
+# reprocess()
+x = np.loadtxt("../Data/train_x_short.csv", delimiter=",") # load from text
+y = np.loadtxt("../Data/train_y.csv", delimiter=",")
+print "Raw:"
+print x[0]
+x = x.reshape(-1, 64, 64) # reshape
+y = y.reshape(-1, 1)
+print "Reshaped"
+print x[0]
+plt.imshow(x[5]) # to visualize only
+plt.show()
